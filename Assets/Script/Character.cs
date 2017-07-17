701a1102f39db951f4434e0fa13652a51c7d960c
HaRu <vharugun9093@gmail.com>
2017-07-17 16:36:26 +0900

    Add Dungeon, Repair Structure

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+    public GameObject pMonster;
+    public float fRange;
+    public float fPower;
+
+    private float fDistance;
+    private float fAngle;
+    private Vector2 vAngle;
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        fDistance = Vector2.Distance(pMonster.GetComponent<Transform>().position, this.GetComponent<Transform>().position);
+
+        if (fDistance <= fRange)
+        {
+            vAngle = pMonster.GetComponent<Transform>().position - this.GetComponent<Transform>().position;
+            vAngle.Normalize();
+            pMonster.GetComponent<Transform>().Translate(Vector2.zero);
+            pMonster.GetComponent<Rigidbody2D>().AddForce(vAngle * fPower);
+        }
+    }
+
+    // ----- Set -----
+
+    public void SetMonster(GameObject gMonster)
+    {
+        pMonster = gMonster;
+    }
+}
---
+++ Assets/Script/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    int hp;
+    Vector3 resetPosition;
+
+    void Start()
+    {
+        resetPosition = new Vector3(0.0f, 6.0f);
+    }
+    
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            DestroyObject(this);
+        }
+
+        if(this.GetComponent<Transform>().position.y >= -6.0f)
+        {
+            Debug.Log("Out");
+            this.GetComponent<Transform>().Translate(resetPosition);
+        }
+	}
+}
